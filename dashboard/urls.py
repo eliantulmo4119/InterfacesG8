42456cd6253a57af90d9b0b8663fe9c39b75d9de
Royce <roger.ugsha9244@utc.edu.ec>
2026-06-10 13:51:08 -0500

    Listar_usuarios

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'private/dashboard.html'),
+    path('listar_usuarios/', views.listar_usuarios, name = 'listar_usuarios'),
 ]
