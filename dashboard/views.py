cab77a1b7c0320d9123d61a69766efa5901f52c6
eliantulmo4119 <elian.tulmo4119@utc.edu.ec>
2026-06-10 13:34:10 -0500

    hola1

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+from django.contrib.auth.models import User
+def listar_usuarios(request):
+    usuarios = User.objects.all()
+    contexto = {
+        'usuarios' : usuarios
+    }
+    return render(request, 'private/listar_usuarios.html', contexto)
