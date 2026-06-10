61eaa268204cabfabe35e5e82d7ef03d9793d986
eliantulmo4119 <elian.tulmo4119@utc.edu.ec>
2026-06-10 13:52:27 -0500

    nose XD

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'private/dashboard.html'),
+    path('private/usuarios/', views.listar_usuarios, name='listar_usuarios'),
+
 ]
