63dab1f2d68e2e75f591e29df8eab52c705694d1
eliantulmo4119 <elian.tulmo4119@utc.edu.ec>
2026-06-10 13:54:20 -0500

    hola

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'private/dashboard.html'),
-<<<<<<< HEAD
-    path('private/usuarios/', views.listar_usuarios, name='listar_usuarios'),
-
-=======
-    path('listar_usuarios/', views.listar_usuarios, name = 'listar_usuarios'),
->>>>>>> 42456cd6253a57af90d9b0b8663fe9c39b75d9de
+    path('listar_usuarios/', views.listar_usuarios, name='listar_usuarios'),
 ]
