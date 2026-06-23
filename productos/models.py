8aa4ecd0efe6e67f392a657e0055378c180bdee0
Royce <roger.ugsha9244@utc.edu.ec>
2026-06-23 13:58:42 -0500

    xd

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(max_length = 100)
+    precio_producto = models.DecimalField(max_digits = 10, decimal_places = 2)
+    stock_producto = models.IntegerField()
+    estado_producto = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        return self.nombre_producto
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.contrib import messages
+
+from django.shortcuts import redirect, render
+from .models import Producto
 
 def listar_productos(request):
-    return render(request, 'listar_productos.html')
+    productos = Producto.objects.all()
+    return render(request, "productos/listar_productos.html", 
+                  {'productos' : productos})
+
+def crear_productos(request):
+    if request.method == "POST":
+        nombre_producto = request.POST.get("nombreProducto") #PONER NOMBRE DEL FORM
+        precio_producto = request.POST.get("precioProducto")
+        stock_producto = request.POST.get("stockProducto")
+        estado_producto = request.POST.get("estadoProducto")
+
+        Producto.objects.create(
+            nombre_producto = nombre_producto,
+            precio_producto = precio_producto,
+            stock_producto = stock_producto,
+            estado_producto = estado_producto
+        )
+        return redirect('listar_productos')
+    return render(request, 'productos/crear_productos.html')
+
+def eliminar_productos(request, id):
+    producto = Producto.objects.get(id = id)
+    producto.delete()
+    messages.success(request, "Producto Eliminado.")
+    return redirect("listar_productos")
+
+def editar_productos(request, id):
+    producto = Producto.objects.get(id = id)
+
+    if request.method == "POST":
+        producto.nombre_producto = request.POST.get("nombreProducto")
+        producto.precio_producto = request.POST.get("precioProducto")
+        producto.stock_producto = request.POST.get("stockProducto")
+        producto.estado_producto = request.POST.get("estadoProducto")
+
+        producto.save()
+        messages.success(request, "Producto Actualizado")
+        return redirect("listar_productos")
+    return render(request, "productos/editar_productos.html", {"producto" : producto})
